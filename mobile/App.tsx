dd35fa8996977366ba9a9f61948d6c0385f1a85a
eliaszlsp <estudoselias@hotmail.com>
2023-05-19 21:14:17 -0300

    Adicionando três pastas ao repositório

---
+++ mobile/App.tsx
@@ -0,0 +1,59 @@
+import { styled } from 'nativewind'
+import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import { SplashScreen } from 'expo-router'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import { StatusBar } from 'expo-status-bar'
+
+const StyledStripes = styled(Stripes)
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return <SplashScreen />
+  }
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 bg-gray-900 px-8"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+        <View className="space-y-2 ">
+          <Text className=" text-center font-title text-2xl leading-tight  text-gray-50 ">
+            Sua cápsula do tempo{' '}
+          </Text>
+          <Text className=" text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity className="rounded-full bg-green-500 px-5 py-3 ">
+          <Text className="font-alt  text-sm uppercase text-black ">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="py-10  text-center font-body text-sm leading-relaxed text-gray-200 ">
+        {' '}
+        Feito com 💜 no NLW da Rocketseat{' '}
+      </Text>
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  )
+}
